191b5b71ce2f721e0c5acc5c7993573119d8a3e4
Daimhim <daimhim@163.com>
2019-08-28 18:38:26 +0800

    18:38:19, 星期三 28, 八月 2019 CST    AM/PM

--- lib/CustomizeWidget.dart
+++ lib/CustomizeWidget.dart
@@ -1,2 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+main()=>runApp(new TitleWidget());
 
 
+class TitleWidget extends StatelessWidget {
+  final GestureTapCallback onTap;
+  final String data;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return new Stack(
+
+      children: <Widget>[
+        new Text("1"),
+        new Text("2"),
+      ],
+    );
+  }
+
+  const TitleWidget(this.data, {
+    this.onTap
+  });
+
+
+  Widget buildTitle() {
+    return new Text()
+  }
+}
+
+class TitleBackWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+}
+
+class _TitleBackWidget extends State<TitleBackWidget> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Icon(Icons.arrow_back);
+  }
+}
